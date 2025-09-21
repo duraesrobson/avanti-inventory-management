8fa01a581d0e8dedc02da7965feeaa32dc04621c
Robson Durães <duraesrobson@gmail.com>
2025-09-21 03:38:14 -0300

    inicio de estrutura da pagina produtos.php

--- produtos.php
+++ produtos.php
@@ -1,12 +1,43 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <!-- link dos icones -->
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
     <title>Dashboard</title>
 </head>
+
 <body>
-    <h1>redirecionado</h1>
+    <header>
+        <div class="header-left">
+            <span class="material-symbols-outlined">
+                deployed_code
+            </span>
+            <h5>
+                Avanti Inventory Management
+            </h5>
+        </div>
+        <div class="header-right">
+            <div class="header-right-conectado">
+                <span class="material-symbols-outlined-dashboard">
+                    dashboard
+                </span>
+                <h6>Conectado</h6>
+            </div>
+            <div class="header-right-logout">
+                <span class="material-symbols-outlined-logout">
+                    logout
+                </span>
+                <h6>Sair</h6>
+            </div>
+        </div>
+    </header>
+    <main>
+
+    </main>
 </body>
+
 </html>
