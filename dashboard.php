708c97c99ffdc79b2cce4da979022db42ce75396
Robson Durães <duraesrobson@gmail.com>
2025-09-21 05:15:24 -0300

    renomear produtos.php para dashboard para melhor coesao

---
+++ dashboard.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <!-- link dos icones -->
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+    <title>Dashboard</title>
+</head>
+
+<body>
+    <header>
+        <div class="header-left">
+            <span class="material-symbols-outlined">
+                deployed_code
+            </span>
+            <h5>
+                Avanti Inventory Management
+            </h5>
+        </div>
+        <div class="header-right">
+            <div class="header-right-conectado">
+                <span class="material-symbols-outlined-dashboard">
+                    dashboard
+                </span>
+                <h6>Conectado</h6>
+            </div>
+            <div class="header-right-logout">
+                <span class="material-symbols-outlined-logout">
+                    logout
+                </span>
+                <h6>Sair</h6>
+            </div>
+        </div>
+    </header>
+    <main class="main-content">
+        <div class="main-topo">
+            <div class="main-topo-text">
+                <h3>
+                    Produtos
+                </h3>
+            </div>
+
+            <div class="main-topo-search">
+                <span class="material-symbols-outlined search-icon">
+                    search
+                </span>
+                <input type="search" name="search" id="main-topo-search-input" placeholder="Buscar por nome...">
+            </div>
+
+            <div class="main-topo-add">
+                <button class="main-topo-add-btn"><span class="material-symbols-outlined-add">
+                        add
+                    </span>
+                    Adicionar Produto</button>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
