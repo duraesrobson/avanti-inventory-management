0daa3196c2a62ee70fce4099a07348b9e7188c1c
Robson Durães <duraesrobson@gmail.com>
2025-09-22 04:54:09 -0300

    adiciona link do modal.css

--- dashboard.php
+++ dashboard.php
@@ -7,6 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/dashboard.css">
+    <link rel="stylesheet" href="css/modal.css">
     <!-- link dos icones -->
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
     <title>Dashboard</title>
