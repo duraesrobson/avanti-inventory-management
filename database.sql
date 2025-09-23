65d370d8d6b4e43e3673acdfb48cdbbdeecc246c
Robson Durães <duraesrobson@gmail.com>
2025-09-23 15:56:19 -0300

    correcao no database.sql adicionando as colunas

--- database.sql
+++ database.sql
@@ -3,11 +3,17 @@ USE estoque_avanti;
 
 CREATE TABLE IF NOT EXISTS usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario VARCHAR(30) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL
+    nome VARCHAR(100) NOT NULL,
+    quantidade INT NOT NULL,
+    preco DECIMAL(10,2) NOT NULL,
+    sku VARCHAR(50),
+    categoria VARCHAR(100),
+    fornecedor VARCHAR(100),
+    descricao TEXT,
+    imagem VARCHAR(255)
 );
 
--- insere um usuario de exemplo para o teste, a senha teria codificacao hash no php
+-- insere um usuario de exemplo para o teste, a senha é "123" e tem codificacao hash no php
 INSERT INTO usuarios (usuario, senha)
 VALUES ('teste@empresa.com', "$2y$12$hRK0r4gNkxWmHRc.7jPRdOnd/NxY5V1cbdCxnuzBW1eFXclGlV7P6%");
 
